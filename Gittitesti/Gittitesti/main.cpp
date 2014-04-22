fcd7496f560e8a5877e7d052bd560c3c70a465ff
Eracage <eracage@gmail.com>
2014-04-22 16:53:43 +0300

    Very much needed changes

--- Gittitesti/Gittitesti/main.cpp
+++ Gittitesti/Gittitesti/main.cpp
@@ -1,7 +1,14 @@
+#include <vector>
+#include <string>
+
 #include "vektori.h"
 #include "test.h"
 
 int main()
 {
+	std::vector<std::string> strings;
+	strings.push_back("asdf");
+	strings.push_back("qwer");
+
 	return 0;
 }
